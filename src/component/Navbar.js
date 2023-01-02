29e1e42e2879b8f0be88b1d8f952a37087044e31
Yoni <ghdrl2648@gmail.com>
2023-01-02 17:10:58 +0900

    side-menu

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,21 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons';
 import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = ({ authenticate, setAuthenticate }) => {
+  let [width, setWidth] = useState(250);
   const navigate = useNavigate();
-  const goToLogin = () => {
-    navigate('/login');
-  }
-  const logout = () => {
-    setAuthenticate(false);
-    navigate('/');
-  }
-  const goToHome = () => {
-    navigate('/');
-  }
   const search = (event) => {
     if (event.key === "Enter") {
       // 입력한 검색어를 읽어와서 url을 바꿔준다.
@@ -27,12 +18,29 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
   
   return (
     <div>
-      <div className='login-button' onClick={authenticate == false ? goToLogin : logout}>
-        <FontAwesomeIcon icon={faUser} />
-        <div>{authenticate == false ? "로그인" : "로그아웃"}</div>
+      <div className="side-menu" style={{ width: width }}>
+        <button className="closebtn" onClick={() => setWidth(0)}>
+          &times;
+        </button>
+        <div className="side-menu-list" id="menu-list">
+          {menuList.map((menu, index) => (
+            <button key={index}>{menu}</button>
+          ))}
+        </div>
       </div>
-      <div className='nav-section' onClick={goToHome}>
-        <img width={100} src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/2560px-H%26M-Logo.svg.png' />
+      <div className="nav-header">
+        <div className="burger-menu hide">
+          <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
+        </div>
+        <div className='login-button' onClick={authenticate == false ? () => navigate('/login') : () => setAuthenticate(false)}>
+          <FontAwesomeIcon icon={faUser} />
+          <div>{authenticate == false ? "로그인" : "로그아웃"}</div>
+        </div>
+      </div>
+      <div className='nav-logo'>
+        <Link to="/">
+          <img width={100} src='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/2560px-H%26M-Logo.svg.png' />
+        </Link>
       </div>
       <div className='menu-area'>
           <ul className='menu-list'>
@@ -42,11 +50,8 @@ const Navbar = ({ authenticate, setAuthenticate }) => {
           </ul>
         <div className='search'>
           <FontAwesomeIcon icon={faSearch} />
-          <input type="text" onKeyPress={search} />
+          <input type="text" placeholder='제품검색' onKeyPress={search} />
         </div>
-        {/* <a href="#" className='menu-toogleBtn'>
-          <i icon={faBars}></i>
-        </a> */}
       </div>
     </div>
   )
